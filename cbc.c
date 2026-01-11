2bfa21f9e19d7315758506e6e494e7fa93ad13ee
Essatara <aya.essatara.06@gmail.com>
2026-01-11 01:10:08 +0100

    Update cbc.c

--- cbc.c
+++ cbc.c
@@ -27,3 +27,23 @@ void encryption(node * head , const unsigned char* key, const unsigned char *iv)
     }
     EVP_CIPHER_CTX_free(ctx);
 }
+void decryption(node * head , const unsigned char* key, const unsigned char *iv)
+{
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    node* current= head;
+    unsigned char previous[16];
+    unsigned char temp[16];
+    memcpy(previous,iv,16);
+    EVP_DecryptInit_ex(ctx,EVP_aes_128_ecb(),NULL,key,NULL);
+    EVP_CIPHER_CTX_set_padding(ctx, 0);
+    while(current!=NULL)
+    {
+        mempcy(temp,current->data,16);
+        int len;
+        EVP_decryptUpdate(ctx,current->data,&len,current->data,16);
+        xor(current->data,previous,current->data);
+        memcpy(previous,temp,16);
+        current=current->nxt;
+    }
+    EVP_CIPHER_CTX_free(ctx);
+}
