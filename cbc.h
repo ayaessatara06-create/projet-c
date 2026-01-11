2101ff82d726d7fc138ebb1691221c83b831b11c
Essatara <aya.essatara.06@gmail.com>
2026-01-11 23:29:40 +0100

    Add comments to function declarations in cbc.h

--- cbc.h
+++ cbc.h
@@ -1,7 +1,7 @@
 #ifndef CBC_H
 #define CBC_H
 #include "linkedlist.h"
-void xor(unsigned char* block1,unsigned char* block2, unsigned char* result);
-void encryption(node* head,const unsigned char* key,const unsigned char*iv);
-void decryption(node* head,const unsigned char* key,const unsigned char*iv);
+void xor(unsigned char* block1,unsigned char* block2, unsigned char* result); //recall of the function xor
+void encryption(node* head,const unsigned char* key,const unsigned char*iv); // recall of the function encryption
+void decryption(node* head,const unsigned char* key,const unsigned char*iv); // recall of the function decryption
 #endif // CBC_H
