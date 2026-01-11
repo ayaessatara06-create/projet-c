f3191e3935dfcd2cada9f2a135538e473a449bc7
Essatara <aya.essatara.06@gmail.com>
2026-01-11 01:34:56 +0100

    Add file I/O functions and node structure

--- fileio.h
+++ fileio.h
@@ -1,1 +1,11 @@
-
+#ifndef FILEIO_H
+#define FILEIO_H
+#include<stdio.h>
+typedef struct node
+{
+    char data;
+    struct node* nxt;
+}node;
+void list_to_file(char* filename,node* head);
+node* file_to_list(char* filename);
+#endif 
