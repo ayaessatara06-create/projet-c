2047c4694b9bc561b68526c09fd92dc3720d6f6e
Essatara <aya.essatara.06@gmail.com>
2026-01-11 01:31:39 +0100

    Update cbc.h

--- cbc.h
+++ cbc.h
@@ -1,1 +1,14 @@
-
+#ifndef CBC_H
+#define CBC_H
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define BLOCK_SIZE 16
+typedef struct 
+{
+    uint8_t iv[BLOCK_SIZE];
+    uint8_t key[BLOCK_SIZE];
+    void (*encryption)(uint8_t *in ,uint8_t *out , uint8_t *key);
+    void (*decryption)(uint8_t *in ,uint8_t *out , uint8_t *key);
+}CBC;
+#endif;
