f06ee5a645cd90e68d972ee19c6ac3be828a159c
Essatara <aya.essatara.06@gmail.com>
2026-01-11 19:26:04 +0100

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -2,11 +2,6 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 #include<stdio.h>
-typedef struct node
-{
-    uint8_t data[16];
-    struct node* nxt;
-}node;
-void list_to_file(char* filename,node* head);
-node* file_to_list(char* filename);
-#endif 
+void list_to_file(char* filename,node* head); //recall of the function list_to_file
+node* file_to_list(char* filename);//recall of the function file_to_list
+#endif // FILEIO_H
