92bfd12287dcaef40e40086e43e1bfac76af4f9a
Essatara <aya.essatara.06@gmail.com>
2026-01-11 02:20:19 +0100

    Update encryption and decryption function signatures

--- cbc.h
+++ cbc.h
@@ -9,7 +9,7 @@ typedef struct
 {
     uint8_t iv[BLOCK_SIZE];
     uint8_t key[BLOCK_SIZE];
-    void (*encryption)(uint8_t *in ,uint8_t *out , uint8_t *key);
-    void (*decryption)(uint8_t *in ,uint8_t *out , uint8_t *key);
+    void (*encryption)(node * head , const unsigned char* key, const unsigned char *iv)
+    void (*decryption)(node * head , const unsigned char* key, const unsigned char *iv)
 }CBC;
 #endif;
